ead17865e2c593908cfe386eff04c3b489f12d47
Chris <rolletchris1@gmail.com>
2024-04-25 14:38:50 -0700

    removed white space

--- service.sql
+++ service.sql
@@ -83,19 +83,16 @@ INSERT INTO `cellphone`.`service_t` (`Service_ID`, `Client_ID`, `Part`, `Service
 INSERT INTO `cellphone`.`service_t` (`Service_ID`, `Client_ID`, `Part`, `Service_Type`, `Cost`) VALUES ('8', '5', 'Headphone Jack', 'Repair', '50.00');
 INSERT INTO `cellphone`.`service_t` (`Service_ID`, `Client_ID`, `Part`, `Service_Type`, `Cost`) VALUES ('9', '6', 'Battery', 'Replace', '45.00');
 INSERT INTO `cellphone`.`service_t` (`Service_ID`, `Client_ID`, `Part`, `Service_Type`, `Cost`) VALUES ('10', '4', 'Back Cover', 'Install', '20.00');
-
 INSERT INTO `cellphone`.`service_t` (`Service_ID`, `Client_ID`, `Part`, `Service_Type`, `Cost`) VALUES ('11', '2', 'Screen', 'Repair', '120.00');
 INSERT INTO `cellphone`.`service_t` (`Service_ID`, `Client_ID`, `Part`, `Service_Type`, `Cost`) VALUES ('12', '1', 'Charging Port', 'Replace', '30.00');
 INSERT INTO `cellphone`.`service_t` (`Service_ID`, `Client_ID`, `Part`, `Service_Type`, `Cost`) VALUES ('13', '3', 'Headphone Jack', 'Repair', '50.00');
 INSERT INTO `cellphone`.`service_t` (`Service_ID`, `Client_ID`, `Part`, `Service_Type`, `Cost`) VALUES ('14', '9', 'Battery', 'Replace', '45.00');
 INSERT INTO `cellphone`.`service_t` (`Service_ID`, `Client_ID`, `Part`, `Service_Type`, `Cost`) VALUES ('15', '10', 'Back Cover', 'Install', '20.00');
-
 INSERT INTO `cellphone`.`service_t` (`Service_ID`, `Client_ID`, `Part`, `Service_Type`, `Cost`) VALUES ('16', '11', 'Screen', 'Repair', '120.00');
 INSERT INTO `cellphone`.`service_t` (`Service_ID`, `Client_ID`, `Part`, `Service_Type`, `Cost`) VALUES ('17', '12', 'Charging Port', 'Replace', '30.00');
 INSERT INTO `cellphone`.`service_t` (`Service_ID`, `Client_ID`, `Part`, `Service_Type`, `Cost`) VALUES ('18', '11', 'Headphone Jack', 'Repair', '50.00');
 INSERT INTO `cellphone`.`service_t` (`Service_ID`, `Client_ID`, `Part`, `Service_Type`, `Cost`) VALUES ('19', '11', 'Battery', 'Replace', '45.00');
 INSERT INTO `cellphone`.`service_t` (`Service_ID`, `Client_ID`, `Part`, `Service_Type`, `Cost`) VALUES ('20', '11', 'Back Cover', 'Install', '20.00');
-
 INSERT INTO `cellphone`.`service_t` (`Service_ID`, `Client_ID`, `Part`, `Service_Type`, `Cost`) VALUES ('21', '4', 'Screen Protector', 'Install', '10.00');
 INSERT INTO `cellphone`.`service_t` (`Service_ID`, `Client_ID`, `Part`, `Service_Type`, `Cost`) VALUES ('22', '5', 'Screen', 'Repair', '120.00');
 INSERT INTO `cellphone`.`service_t` (`Service_ID`, `Client_ID`, `Part`, `Service_Type`, `Cost`) VALUES ('23', '6', 'Charging Port', 'Replace', '30.00');
